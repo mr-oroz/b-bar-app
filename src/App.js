63416eb87384c3eedf83790f4f24ee6ffe4db947
Muralimjanov <muralimjanov@mail.ru>
2021-12-07 19:37:57 +0600

    slide

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Slider from './components/Carousel/Slider';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Slider />
     </div>
   );
 }
