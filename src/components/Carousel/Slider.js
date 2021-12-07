9ed560895fc8ea5007ada3435c23e3d0fa67cf8f
@oroz7308 <oroz7307@gmail.com>
2021-12-07 21:04:52 +0600

    dwad

--- src/components/Carousel/Slider.js
+++ src/components/Carousel/Slider.js
@@ -13,10 +13,7 @@ const Slider = () => {
                         src={photo}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -25,10 +22,7 @@ const Slider = () => {
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -36,11 +30,6 @@ const Slider = () => {
                         src={photo}
                         alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
