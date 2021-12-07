f39f5eee5c1ef26610435ecd8b388f5acfc5c4a4
@oroz7308 <oroz7307@gmail.com>
2021-12-07 19:16:17 +0600

    merge branch

--- src/components/FooterComponent/About-us/about.js
+++ src/components/FooterComponent/About-us/about.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import img from '../../../assets/img/about-us.png'
+import img from '../../../Assets/img/about.png';
 
 const About = () => {
     return (
-        <div style={{backgroundColor:'black',color:'white'}}>
-            <h1 style={{marginLeft:'40px'}}>О нас</h1>
-            <div className="about_inner" style={{display:'flex'}}>
-                <img src={img} alt="" style={{width:'500px',height:'350px',marginLeft:'40px'}} />
-                <p style={{margin:'20px'}}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+        <div style={{ color: 'white' }}>
+            <h1 style={{ marginLeft: '40px' }}>О нас</h1>
+            <div className="about_inner" style={{ display: 'flex' }}>
+                <img src={img} alt="" style={{ width: '500px', height: '350px', marginLeft: '40px' }} />
+                <p style={{ margin: '20px' }}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
             </div>
         </div>
     );
--- src/components/FooterComponent/Footer.js
+++ src/components/FooterComponent/Footer.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import Logo from '../../assets/img/Лого.png'
-import inst from '../../assets/img/inst.png'
-import what from '../../assets/img/what.png'
-import face from '../../assets/img/face.png'
-
+import Logo from '../../Assets/img/Лого.png'
+import inst from '../../Assets/img/inst.png'
+import what from '../../Assets/img/what.png'
+import face from '../../Assets/img/face.png'
+import './Footer.css'
 const Footer = () => {
     return (
-        <div className = "Footer" style={{display: "flex",justifyContent:'space-around',alignItems:'center',
-        backgroundColor:'black',color:'white',width:'100%',height:'200px'}}>
+        <div className="Footer"
+            style={{
+                display: "flex",
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                color: 'white',
+                width: '100%',
+                height: '200px'
+            }}>
             <div className="footer_logo">
-                <img src={Logo} alt="" style={{width:'150px', height:'150px'}} />
+                <img src={Logo} alt=""
+                    style={{
+                        width: '150px',
+                        height: '150px'
+                    }} />
             </div>
             <div className="rezhim">
                 <h3>Режим работы</h3>
@@ -26,10 +37,16 @@ const Footer = () => {
                 <p>г.Бишкек, Медерова 50/2</p>
                 <p>brr</p>
             </div>
-            <div className="icons" style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
-                <img src={inst} alt="" style={{width:'30px',height:'30px'}} />
-                <img src={what} alt="" style={{width:'25px',height:'25px',margin:'20px'}} />
-                <img src={face} alt="" style={{width:'18px',height:'29px'}} />
+            <div className="icons" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+               <div className='icon'>
+                   <img src={inst}/>
+               </div>
+               <div className='icon'>
+                   <img src={what}/>
+               </div>
+               <div className='icon'>
+                   <img src={face}/>
+               </div>
             </div>
         </div>
     );
