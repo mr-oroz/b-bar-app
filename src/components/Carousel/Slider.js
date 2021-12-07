667d641a6469e499ce5ca5da312265ba8a1a8bd3
Muralimjanov <muralimjanov@mail.ru>
2021-12-07 20:14:52 +0600

    slider

--- src/components/Carousel/Slider.js
+++ src/components/Carousel/Slider.js
@@ -11,36 +11,22 @@ const Slider = () => {
                     <img
                         className="d-block w-100"
                         src={photo}
-                        alt="First slide"
+                        alt=""
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={photo}
-                        alt="Second slide"
+                        alt=""
                     />
-
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={photo}
-                        alt="Third slide"
+                        alt=""
                     />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
