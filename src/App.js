ed529147e3ae9d5013ebfc5431ad3368f6d2357e
Ernest <joomartuuluernest@gmail.com>
2021-12-07 18:22:47 +0600

    Footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './App.css';
 import Footer from './components/FooterComponent/Footer'
 import { Container } from 'react-bootstrap';
-import About from './components/FooterComponent/About us'
+import About from './components/FooterComponent/About us/about'
 
 
 const App = () => {
   return (
     <div className="App">
-      <Container>
-        <About />
-        <Footer />
-      </Container>
+        <Container>
+          <About/>
+          <Footer />
+        </Container>
     </div>
   );
 }
